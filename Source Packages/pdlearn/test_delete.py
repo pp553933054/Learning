ef500a3e17f7e9d8a6ba30bd8d041dff3d00c03e
yangxinghui <pp553933054@sina.com>
2019-08-06 09:55:49 +0800

    测试文件

---
+++ Source Packages/pdlearn/test_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019-08-06 9:54 
+# @Author : YXH
+# @Site :  
+# @File : test_delete.py 
+# @Software: PyCharm
+
+def get_detail():
+    pass
